9c9c805b14845aa130c4a9b8b38d54c3552858e5
jason-francois <jasonc.francois@gmail.com>
2023-05-26 19:56:17 -0400

    Started About component

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,3 +1,18 @@
+import styles from "../styles/About.module.css";
 export default function About() {
-  return <></>;
+  return (
+    <>
+      <div>
+        <h1
+          className={`${styles["projects__header"]} block text-center text-4xl font-bold mb-3`}
+        >
+          <span
+            className={`${styles["projects__header-content"]} section__header relative border-solid pb-1`}
+          >
+            About Me
+          </span>
+        </h1>
+      </div>
+    </>
+  );
 }
