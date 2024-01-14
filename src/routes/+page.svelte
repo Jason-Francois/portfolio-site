d735e8e8fd4c7708892f50cc0420b4607086edfc
Jason Francois <jasonc.francois@gmail.com>
2024-01-14 17:21:46 -0500

    added tailwind, updated resume

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,2 @@
 <h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<p class="text-violet-600" >Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
